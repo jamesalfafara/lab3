4243bde69198f37c47ebff9ab2b3908c25860f38
James Alfafara <jsalfafara@student.apc.edu.ph>
2023-03-04 15:08:12 +0800

    Create guest entry based on CI4 create new item tutorial

--- ci4/app/Models/GuestModel.php
+++ ci4/app/Models/GuestModel.php
@@ -8,6 +8,8 @@ class GuestModel extends Model
 {
 	protected $table = 'myguests';
 
+	protected $allowedFields = ['name', 'email', 'website', 'comment', 'gender'];
+
 	public function getGuest()
 	{
 		return $this->findAll();
